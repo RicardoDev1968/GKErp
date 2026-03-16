00c9b36c909021906e6cc1a62b7e73ee56b031cd
Ricardo Da Silva <81180-RICARDO@smig.local>
2026-03-16 17:57:01 -0400

    primeiro commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <v-container fluid class="mt-1">
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-card-title>IGDIRECIONADOR</v-card-title>
+           <v-divider></v-divider>
+
+          <v-card-text>Pesquisar...</v-card-text>
+
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="blue">
+              <v-icon color="black" left>
+                mdi-magnify
+              </v-icon>
+              Pesquisar
+            </v-btn>
+             <v-btn color ="white">
+              <v-icon color="black" left>
+                mdi-filter-remove
+              </v-icon>
+              Limpar Filtros
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+ </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  name: 'HomePage'
 }
 </script>
